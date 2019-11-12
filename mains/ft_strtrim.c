edbfdb888306967b54460ba67b175a575b3795f9
Paloma MOUHALI <pmouhali@e1r6p17.42.fr>
2019-11-12 12:11:47 +0100

    better main for memory check

--- mains/ft_strtrim.c
+++ mains/ft_strtrim.c
@@ -7,9 +7,14 @@ void	ft_putendl(char *s) { write(1, s, ft_strlen(s)); write(1, "\n", 1); }
 int		main(void)
 {
 	char *s;
+	char *e;
+	char *set;
+
+	set = ft_strdup(" x6");
 
 ft_putstr("with set included characters at start and end of string : ");
-	s = ft_strtrim(" x6HELL666HELL6x ", " x6");
+	e = ft_strdup(" x6HELL666HELL6x ");
+	s = ft_strtrim(e, set);
 	if (strcmp(s, "HELL666HELL"))
 	{
 		ft_putstr("KO : ");
@@ -18,9 +23,11 @@ ft_putstr("with set included characters at start and end of string : ");
 	else
 		ft_putendl("OK");
 	free(s);
+	free(e);
 
 ft_putstr("   with set included characters only at start of string : ");
-	s = ft_strtrim(" x6HELL666HELL", " x6");
+	e = ft_strdup(" x6HELL666HELL");
+	s = ft_strtrim(e, set);
 	if (strcmp(s, "HELL666HELL"))
 	{
 		ft_putstr("KO : ");
@@ -29,9 +36,11 @@ ft_putstr("   with set included characters only at start of string : ");
 	else
 		ft_putendl("OK");
 	free(s);
+	free(e);
 
 ft_putstr("     with set included characters only at end of string : ");
-	s = ft_strtrim("HELL666HELL6x ", " x6");
+	e = ft_strdup("HELL666HELL6x ");
+	s = ft_strtrim(e, set);
 	if (strcmp(s, "HELL666HELL"))
 	{
 		ft_putstr("KO : ");
@@ -40,9 +49,11 @@ ft_putstr("     with set included characters only at end of string : ");
 	else
 		ft_putendl("OK");
 	free(s);
+	free(e);
 
 ft_putstr("        with set included characters only in the middle : ");
-	s = ft_strtrim("HELL6 xx6 xx6xxx HELL", " x6");
+	e = ft_strdup("HELL6 xx6 xx6xxx HELL");
+	s = ft_strtrim(e, set);
 	if (strcmp(s, "HELL6 xx6 xx6xxx HELL"))
 	{
 		ft_putstr("KO : ");
@@ -51,17 +62,23 @@ ft_putstr("        with set included characters only in the middle : ");
 	else
 		ft_putendl("OK");
 	free(s);
+	free(e);
 
 ft_putstr("     with a string only made of set included characters : ");
-	s = ft_strtrim(" 666x xx xxx6", " x6");
+	e = ft_strdup(" 666x xx xxx6");
+	s = ft_strtrim(e, set);
 	if (strcmp(s, ""))
 		ft_putendl("KO : does not return an empty string");
 	else
 		ft_putendl("OK");
 	free(s);
+	free(e);
 
+	free(set);
 ft_putstr("                           with an empty characters set : ");
-	s = ft_strtrim(" x6HELL666HELL6x ", "");
+	e = ft_strdup(" x6HELL666HELL6x ");
+	set = ft_strdup("");
+	s = ft_strtrim(e, set);
 	if (strcmp(s, " x6HELL666HELL6x "))
 	{
 		ft_putstr("KO : ");
@@ -70,22 +87,21 @@ ft_putstr("                           with an empty characters set : ");
 	else
 		ft_putendl("OK");
 	free(s);
+	free(e);
 	
+	free(set);
+
 ft_putstr("                                   with an empty string : ");
-	s = ft_strtrim("", "");
+	e = ft_strdup("");
+	set = ft_strdup("6x ");
+	s = ft_strtrim(e, "");
 	if (strcmp(s, ""))
 		ft_putendl("KO : does not return an empty string");
 	else
 		ft_putendl("OK");
 	free(s);
 
-ft_putstr("                                  with a NULL parameter : ");
-	s = ft_strtrim("HELL", NULL);
-	if (s)
-		ft_putendl("KO : does not return an empty string");
-	else
-		ft_putendl("OK");
-	free(s);
+	free(set);
 }
 
 /* Be careful to allocate only the required space ! */
